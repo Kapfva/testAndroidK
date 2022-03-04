e0408166a9e267d60341624c5d3f9ac14b477da7
Kai-Master <kpfva@hotmail.com>
2022-03-04 15:00:35 -0500

    Change person class to kotline class.

--- app/src/main/java/com/example/testandroidk/model/Person.java
+++
@@ -1,9 +0,0 @@
-package com.example.testandroidk.model;
-
-public class Person {
-    public String name;
-    public String age;
-    public String contact;
-    public Boolean active;
-
-}
---
+++ app/src/main/java/com/example/testandroidk/model/Person.kt
@@ -0,0 +1,5 @@
+package com.example.testandroidk.model
+
+data class Person (var name : String ?= null, var birthdate : String ?= null,var contactNumber : String ?= null, var active : Boolean) {
+
+}
