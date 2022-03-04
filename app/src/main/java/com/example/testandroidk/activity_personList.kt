f82f4a1403601cc0eb3161295bb67bc4b7c28af5
Kai-Master <kpfva@hotmail.com>
2022-03-04 17:02:35 -0500

    Get from Database

--- app/src/main/java/com/example/testandroidk/activity_personList.kt
+++ app/src/main/java/com/example/testandroidk/activity_personList.kt
@@ -1,11 +1,44 @@
 package com.example.testandroidk
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.testandroidk.model.Person
+import com.google.firebase.database.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class activity_personList : AppCompatActivity() {
+
+    private val db = Firebase.firestore
+    private lateinit var personRecyclerView: RecyclerView
+    private lateinit var personMutableList: MutableList<Person>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_person_list)
+
+        personRecyclerView = findViewById<RecyclerView>(R.id.rvPersonItems)
+        personRecyclerView.layoutManager= LinearLayoutManager(this)
+        personRecyclerView.setHasFixedSize(true)
+
+        personMutableList= mutableListOf<Person>()
+        getPersonData()
+    }
+
+    private fun getPersonData() {
+
+        val dbref = db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+
+                }
+            }
+            .addOnFailureListener { exception ->
+            }
+
     }
 }
---
+++ app/src/main/java/com/example/testandroidk/actvity_Map.kt
@@ -0,0 +1,11 @@
+package com.example.testandroidk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class actvity_Map : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actvity_map)
+    }
+}
